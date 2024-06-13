cfe42be5eb20b4332d094616a8e3f4aa1c49708c
Eetu Laukka <eetu_laukka@hotmail.com>
2024-06-13 15:39:55 +0300

    State machine with some sounds added and simple lighting blackout

--- Assets/moveBall.cs
+++ Assets/moveBall.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class moveBall : MonoBehaviour
 {
+    public GameObject player;
+    public AudioSource woobwoob;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +14,10 @@ public class moveBall : MonoBehaviour
     void Update()
     {
         transform.localPosition=new Vector3(-6.1f , 9.8f , transform.localPosition.z -3 * Time.deltaTime);
+        float distance = Vector3.Distance (transform.position, player.transform.position);
+        Debug.Log(distance);
+        if (distance < 100 && !woobwoob.isPlaying){
+            woobwoob.Play();
+        }
     }
 }
