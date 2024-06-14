5413f587feb33f2f2d85f9ef6c7030fbf661e503
Martin Hedlund <martinhedlund@live.com>
2024-06-14 09:42:06 +0300

    WIP Locomotion

--- Assets/BroomController.cs
+++ Assets/BroomController.cs
@@ -7,19 +7,22 @@ using UnityEditor;
 using TextMappings;
 using UnityEngine.InputSystem;
 
-public enum SteeringMethod {
+public enum SteeringMethod
+{
     PointingMethod,
     LeaningMethod,
     HeadMethod
 }
 
-public enum SpeedMethod {
+public enum SpeedMethod
+{
     ControllerMethod,
     ControllerHeadMethod,
     ControllerBodyMethod
 }
 
-public enum Invert {
+public enum Invert
+{
     yes = -1,
     no = 1,
 }
@@ -37,22 +40,29 @@ public class BroomController : MonoBehaviour
     [Tooltip("If true, previous locomotion is used to compute current locomoton")]
     public bool usePrevious = true;
 
- 
+
 
 
     public SpeedMethod selectedSpeedMethod = SpeedMethod.ControllerMethod;
 
-    private SpecificSpeedMethod _speedMethod { get {
-        if (selectedSpeedMethod == SpeedMethod.ControllerMethod) {
-            return ControllerMethod;
+    private SpecificSpeedMethod _speedMethod
+    {
+        get
+        {
+            if (selectedSpeedMethod == SpeedMethod.ControllerMethod)
+            {
+                return ControllerMethod;
+            }
+            else if (selectedSpeedMethod == SpeedMethod.ControllerHeadMethod)
+            {
+                return ControllerHeadMethod;
+            }
+            else
+            {
+                return ControllerBodyMethod;
+            }
         }
-        else if (selectedSpeedMethod == SpeedMethod.ControllerHeadMethod) {
-            return ControllerHeadMethod;
-        }
-        else {
-            return ControllerBodyMethod;
-        }
-    }}
+    }
 
     [SerializeField] public ControllerMethod ControllerMethod;
     [SerializeField] public ControllerHeadMethod ControllerHeadMethod;
@@ -63,17 +73,24 @@ public class BroomController : MonoBehaviour
 
     public SteeringMethod selectedSteeringMethod = SteeringMethod.PointingMethod;
 
-    private SpecificMethod _steeringMethod { get {
-        if (selectedSteeringMethod == SteeringMethod.PointingMethod) {
-            return PointingMethod;
+    private SpecificMethod _steeringMethod
+    {
+        get
+        {
+            if (selectedSteeringMethod == SteeringMethod.PointingMethod)
+            {
+                return PointingMethod;
+            }
+            else if (selectedSteeringMethod == SteeringMethod.LeaningMethod)
+            {
+                return LeaningMethod;
+            }
+            else
+            {
+                return HeadMethod;
+            }
         }
-        else if (selectedSteeringMethod == SteeringMethod.LeaningMethod) {
-            return LeaningMethod;
-        }
-        else {
-            return HeadMethod;
-        }
-    }}
+    }
 
     [SerializeField] public PointingMethod PointingMethod;
     [SerializeField] public LeaningMethod LeaningMethod;
@@ -102,16 +119,18 @@ public class BroomController : MonoBehaviour
 
     void Start()
     {
-        _offset = head.transform.localPosition - localReference.localPosition; 
+        _offset = head.transform.localPosition - localReference.localPosition;
 
         _allowMovement = actions.FindActionMap("XRI RightHand Locomotion").FindAction("Grab Move");
-        if (_allowMovement != null) {
+        if (_allowMovement != null)
+        {
             Debug.Log("allow move");
         }
-        
+
         _setOffset = actions.FindActionMap("XRI RightHand Interaction").FindAction("Offset");
 
-        if (_setOffset != null) {
+        if (_setOffset != null)
+        {
             Debug.Log("allow offset");
         }
         _rightControllerPosition = actions.FindActionMap("XRI RightHand").FindAction("Position");
@@ -120,64 +139,82 @@ public class BroomController : MonoBehaviour
         InfoBoard.Instance.FollowMe(gameObject);
     }
 
-    public void UseControllerSpeed() {
+    public void UseControllerSpeed()
+    {
         selectedSpeedMethod = SpeedMethod.ControllerMethod;
         InfoBoard.Instance.SetSpeedMethodText("Controller");
     }
 
-    public void UseHeadSpeed() {
+    public void UseHeadSpeed()
+    {
         selectedSpeedMethod = SpeedMethod.ControllerHeadMethod;
         InfoBoard.Instance.SetSpeedMethodText("Head");
     }
 
-    public void UseBodySpeed() {
+    public void UseBodySpeed()
+    {
         selectedSpeedMethod = SpeedMethod.ControllerBodyMethod;
         InfoBoard.Instance.SetSpeedMethodText("Body");
     }
 
-    public void UsePointerSteering() {
+    public void UsePointerSteering()
+    {
         selectedSteeringMethod = SteeringMethod.PointingMethod;
         InfoBoard.Instance.SetSteeringMethodText("Pointing");
     }
-    public void UseHeadSteering() {
+    public void UseHeadSteering()
+    {
         selectedSteeringMethod = SteeringMethod.HeadMethod;
         InfoBoard.Instance.SetSpeedMethodText("Head");
     }
-    public void UseLeanSteering() {
+    public void UseLeanSteering()
+    {
         selectedSteeringMethod = SteeringMethod.LeaningMethod;
         InfoBoard.Instance.SetSpeedMethodText("Leaning");
     }
-    public void SetInvertHorizontal(bool b) {
-        if (b) {
+    public void SetInvertHorizontal(bool b)
+    {
+        if (b)
+        {
             invertHorizontal = Invert.yes;
         }
-        else {
+        else
+        {
             invertHorizontal = Invert.no;
         }
     }
 
-     public void SetInvertLateral(bool b) {
-        if (b) {
+    public void SetInvertLateral(bool b)
+    {
+        if (b)
+        {
             invertLateral = Invert.yes;
         }
-        else {
+        else
+        {
             invertLateral = Invert.no;
         }
     }
-    public void SetInvertVertical(bool b) {
-        if (b) {
+    public void SetInvertVertical(bool b)
+    {
+        if (b)
+        {
             invertVertical = Invert.yes;
         }
-        else {
+        else
+        {
             invertVertical = Invert.no;
         }
     }
 
-    public void SetInvertForward(bool b) {
-        if (b) {
+    public void SetInvertForward(bool b)
+    {
+        if (b)
+        {
             invertForward = Invert.yes;
         }
-        else {
+        else
+        {
             invertForward = Invert.no;
         }
     }
@@ -193,20 +230,22 @@ public class BroomController : MonoBehaviour
             SetLateralMovement();
             SetVerticalMovement();
             SetForwardMovement();
-            
+
         }
-        else if (_setOffset.IsPressed()){
+        else if (_setOffset.IsPressed())
+        {
             // OVRInput.Get(OffsetButton)
             Debug.Log("Set offset");
-            _offset = head.transform.localPosition - localReference.localPosition; 
+            _offset = head.transform.localPosition - localReference.localPosition;
         }
 
-        
-        
+
+
     }
 
 
-    void SetHorizontalMovement() {
+    void SetHorizontalMovement()
+    {
         InputMode inputMode = _steeringMethod.HorizontalInputMode;
         InputSource inputSource = _steeringMethod.HorizontalInputSource;
         RotationAxis axis = _steeringMethod.HorizontaltAxis;
@@ -214,230 +253,283 @@ public class BroomController : MonoBehaviour
         Vector3 outputAxis = transform.up;
         float value = 0;
 
-        if (inputMode == InputMode.Angle) {
+        if (inputMode == InputMode.Angle)
+        {
             Quaternion input = GetInputOrientation(inputSource);
             value = GetAngle(input, axis);
         }
-        else if (inputMode == InputMode.Distance) { 
+        else if (inputMode == InputMode.Distance)
+        {
             Vector3 input = GetInputPosition(inputSource);
             value = GetDistance(input, axis);
         }
-        else {
+        else
+        {
             return;
         }
 
         float sensitivity;
-        if (usePrevious) {
+        if (usePrevious)
+        {
             sensitivity = _steeringMethod.rotationSensitivityUsePrevious;
         }
-        else {
+        else
+        {
             sensitivity = _steeringMethod.rotationSensitivity;
         }
 
-        value = (float) invertHorizontal * value;
+        value = (float)invertHorizontal * value;
 
         TranslateOrRotate(outputMode, outputAxis, value, sensitivity);
 
     }
 
-    void SetLateralMovement() {
+    void SetLateralMovement()
+    {
         InputMode inputMode = _steeringMethod.LateralInputMode;
         InputSource inputSource = _steeringMethod.LateralInputSource;
         RotationAxis axis = _steeringMethod.LateralAxis;
         OutputMode outputMode = _steeringMethod.LateralOutputMode;
         Vector3 outputAxis = transform.right;
-        
+
         float value;
 
-        if (inputMode == InputMode.Angle) {
+        if (inputMode == InputMode.Angle)
+        {
             Quaternion input = GetInputOrientation(inputSource);
             value = GetAngle(input, axis);
         }
-        else if (inputMode == InputMode.Distance) {
+        else if (inputMode == InputMode.Distance)
+        {
             Vector3 input = GetInputPosition(inputSource);
             value = GetDistance(input, axis);
         }
-        else {
+        else
+        {
             return;
         }
 
         float sensitivity;
-        if (usePrevious) {
+        if (usePrevious)
+        {
             sensitivity = _steeringMethod.lateralSensitivityUsePrevious;
         }
-        else {
+        else
+        {
             sensitivity = _steeringMethod.lateralSensitivity;
         }
 
-        value = (float) invertLateral * value;
+        value = (float)invertLateral * value;
+
+        //Vector3 newAxis = new Vector3(0, _rightControllerRotation.ReadValue<Quaternion>().eulerAngles.y, 0).normalized;
 
         TranslateOrRotate(outputMode, outputAxis, value, sensitivity);
 
     }
 
-    void SetVerticalMovement() {
+    void SetVerticalMovement()
+    {
         InputMode inputMode = _steeringMethod.VerticalInputMode;
         InputSource inputSource = _steeringMethod.VerticalInputSource;
         RotationAxis axis = _steeringMethod.VerticalAxis;
-        
+
         float value;
 
-        if (inputMode == InputMode.Angle) {
+        if (inputMode == InputMode.Angle)
+        {
             Quaternion input = GetInputOrientation(inputSource);
             value = GetAngle(input, axis);
         }
-        else if (inputMode == InputMode.Distance) {
+        else if (inputMode == InputMode.Distance)
+        {
             Vector3 input = GetInputPosition(inputSource);
             value = GetDistance(input, axis);
         }
-        else {
+        else
+        {
             return;
         }
 
         float sensitivity;
-        if (usePrevious) {
+        if (usePrevious)
+        {
             sensitivity = _steeringMethod.upDownSensitivityUsePrevious;
         }
-        else {
+        else
+        {
             sensitivity = _steeringMethod.upDownSensitivity;
         }
 
-        value = (float) invertVertical * value;
+        value = (float)invertVertical * value;
 
         TranslateOrRotate(_steeringMethod.VerticalOutputMode, transform.up, value, sensitivity);
     }
 
 
-    void SetForwardMovement() {
+    void SetForwardMovement()
+    {
 
         float rawInput = 0;
 
-        if (selectedSpeedMethod == SpeedMethod.ControllerMethod) {
+        if (selectedSpeedMethod == SpeedMethod.ControllerMethod)
+        {
             //rawInput = OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger);
             rawInput = _allowMovement.ReadValue<float>();
-            
+
         }
-        else if (selectedSpeedMethod == SpeedMethod.ControllerHeadMethod) {
+        else if (selectedSpeedMethod == SpeedMethod.ControllerHeadMethod)
+        {
             Vector3 controllerPosition = GetInputPosition(InputSource.Controller);
             Vector3 headPosition = GetInputPosition(InputSource.Head);
 
-            rawInput =  Mathf.Clamp(1.2f - (headPosition - controllerPosition).magnitude, 0, 1);
+            rawInput = Mathf.Clamp(1.2f - (headPosition - controllerPosition).magnitude, 0, 1);
         }
-        else {
+        else
+        {
             Vector3 controllerPosition = GetInputPosition(InputSource.Controller);
             rawInput = GetDistance(controllerPosition, RotationAxis.Roll);
         }
 
-        rawInput = (float) invertForward * rawInput;
+        rawInput = (float)invertForward * rawInput;
 
-        if (usePrevious) {
-              Translate(rawInput, transform.forward, _speedMethod.usePreviousAmplification);
-            }
-            else {
-                Translate(rawInput, transform.forward, _speedMethod.amplification);
+        if (usePrevious)
+        {
+            Translate(rawInput, transform.forward, _speedMethod.usePreviousAmplification);
+        }
+        else
+        {
+            Translate(rawInput, transform.forward, _speedMethod.amplification);
         }
     }
-    void TranslateOrRotate(OutputMode outputMode, Vector3 outputAxis, float value, float sensitivity) {
+    void TranslateOrRotate(OutputMode outputMode, Vector3 outputAxis, float value, float sensitivity)
+    {
 
-        if (outputMode == OutputMode.Angle) {
+        if (outputMode == OutputMode.Angle)
+        {
             Rotate(value, outputAxis, sensitivity);
         }
-        else {
+        else
+        {
             Translate(value, outputAxis, sensitivity);
         }
     }
 
-    Quaternion GetInputOrientation(InputSource sourceType) {
-        if (sourceType == InputSource.Controller) {
-           // return OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTouch);
-           return _rightControllerRotation.ReadValue<Quaternion>();
+    Quaternion GetInputOrientation(InputSource sourceType)
+    {
+        if (sourceType == InputSource.Controller)
+        {
+            // return OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTouch);
+            return _rightControllerRotation.ReadValue<Quaternion>();
         }
-        else if (sourceType == InputSource.Head) {
+        else if (sourceType == InputSource.Head)
+        {
             return head.transform.localRotation;
         }
-        else {
+        else
+        {
             return Quaternion.identity;
         }
     }
 
-    Vector3 GetInputPosition(InputSource sourceType) {
-        if (sourceType == InputSource.Controller) {
+    Vector3 GetInputPosition(InputSource sourceType)
+    {
+        if (sourceType == InputSource.Controller)
+        {
             //return OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
             return _rightControllerPosition.ReadValue<Vector3>();
         }
-        else if (sourceType == InputSource.Head) {
+        else if (sourceType == InputSource.Head)
+        {
             return head.transform.localPosition;
         }
-        else {
+        else
+        {
             return Vector3.zero;
         }
     }
 
 
-    float GetAngle(Quaternion inputLocalOrientation, RotationAxis axis) {
-        if (axis == RotationAxis.Yaw) {
+    float GetAngle(Quaternion inputLocalOrientation, RotationAxis axis)
+    {
+        if (axis == RotationAxis.Yaw)
+        {
 
-            return  Quaternion.Angle(
+            return Quaternion.Angle(
             Quaternion.Euler(0f, localReference.localEulerAngles.y, 0f),
             Quaternion.Euler(0f, inputLocalOrientation.eulerAngles.y, 0f)
-            ) *  Mathf.Sign(inputLocalOrientation.y);
+            ) * Mathf.Sign(inputLocalOrientation.y);
         }
-        else if (axis == RotationAxis.Pitch) {
+        else if (axis == RotationAxis.Pitch)
+        {
             return Quaternion.Angle(
             Quaternion.Euler(localReference.localEulerAngles.x, 0f, 0f),
-            Quaternion.Euler(inputLocalOrientation.eulerAngles.x,0f, 0f)
-            ) *  -1 * Mathf.Sign(inputLocalOrientation.x);
+            Quaternion.Euler(inputLocalOrientation.eulerAngles.x, 0f, 0f)
+            ) * -1 * Mathf.Sign(inputLocalOrientation.x);
         }
-        else if (axis == RotationAxis.Roll) {
-            return  Quaternion.Angle(
+        else if (axis == RotationAxis.Roll)
+        {
+            return Quaternion.Angle(
             Quaternion.Euler(0f, 0f, localReference.localEulerAngles.z),
             Quaternion.Euler(0f, 0f, inputLocalOrientation.eulerAngles.z)
-            ) *  Mathf.Sign(inputLocalOrientation.z);
+            ) * Mathf.Sign(inputLocalOrientation.z);
         }
         return 0;
     }
 
-   float GetDistance(Vector3 inputLocalPosition, RotationAxis axis) {
+    float GetDistance(Vector3 inputLocalPosition, RotationAxis axis)
+    {
 
-        if (axis == RotationAxis.Yaw) {
-            return inputLocalPosition.y - localReference.localPosition.y;
+        if (axis == RotationAxis.Yaw)
+        {
+            Debug.Log("localReference position: " + _offset.y);
+            Debug.Log("input localPosition: " + inputLocalPosition.z);
+            return inputLocalPosition.y - _offset.y;
         }
-        else if (axis == RotationAxis.Pitch) {
+        else if (axis == RotationAxis.Pitch)
+        {
             return inputLocalPosition.x - localReference.localPosition.x;
         }
-        else if (axis == RotationAxis.Roll) {
-            return  inputLocalPosition.z - localReference.localPosition.z;
+        else if (axis == RotationAxis.Roll)
+        {
+
+            return inputLocalPosition.z - localReference.localPosition.z;
         }
         return 0;
     }
 
 
 
-    void Rotate(float amount, Vector3 axis, float sensitivity) {
+    void Rotate(float amount, Vector3 axis, float sensitivity)
+    {
 
         float frameScalar = 70 * Time.fixedDeltaTime;
 
-        if (usePrevious) {
+        if (usePrevious)
+        {
             gameObject.GetComponent<Rigidbody>().AddTorque(axis * amount * sensitivity * frameScalar);
         }
-        else {
+        else
+        {
             transform.Rotate(axis, amount * sensitivity * frameScalar, Space.World);
         }
-     }
+    }
 
-    void Translate(float amount, Vector3 axis, float sensitivity) {
+    void Translate(float amount, Vector3 axis, float sensitivity)
+    {
 
         float frameScalar = 70 * Time.fixedDeltaTime;
 
-        if (usePrevious) {
-            gameObject.GetComponent<Rigidbody>().AddForce(axis * amount * sensitivity * frameScalar);
+        if (usePrevious)
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(amount * sensitivity * frameScalar * axis);
         }
-        else {
-            transform.Translate(axis * amount * sensitivity * frameScalar, Space.World);
+        else
+        {
+            transform.Translate(amount * sensitivity * frameScalar * axis, Space.World);
         }
 
-     }
-    
+    }
+
 
 
 
